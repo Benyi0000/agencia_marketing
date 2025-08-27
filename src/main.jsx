dddc0abba26426961cc18f9522931ae50a48040f
Benyi0000 <benjamin_benitez@outlook.com>
2025-08-26 21:08:03 -0300

    Primer commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// src/main.jsx
+import React from 'react'                      // 👈 importa React
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
